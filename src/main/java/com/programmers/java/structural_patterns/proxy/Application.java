e2ca2cc99bd6901b664bdd335b2575580546660b
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-11-12 01:20:39 +0900

    fix: 프록시 패턴의 시뮬레이션 순서를 바꾸다.

--- src/main/java/com/programmers/java/structural_patterns/proxy/Application.java
+++ src/main/java/com/programmers/java/structural_patterns/proxy/Application.java
@@ -11,8 +11,8 @@ public class Application {
 
     static Map<Integer, Simulation> map = new HashMap<>() {{
         put(1, new SimulationLogging());
-        put(2, new SimulationVirtual());
-        put(3, new SimulationProtection());
+        put(2, new SimulationProtection());
+        put(3, new SimulationVirtual());
     }};
 
     public static void main(String[] args) {
@@ -23,12 +23,12 @@ public class Application {
         simulation.run();
         simulation.jump();
 
-        /******** 가상 프록시 *******/
+        /******** 보호 프록시 *******/
         simulation = map.get(2);
         simulation.run();
         simulation.jump();
 
-        /******** 보호 프록시 *******/
+        /******** 가상 프록시 *******/
         simulation = map.get(3);
         simulation.run();
         simulation.jump();
